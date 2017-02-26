b868b3895307835506dc63c13c570152e8705ea2
Robert Snyder <immeraufdemhund@hotmail.com>
2017-02-26 14:56:13 -0600

    updated jelly file for PicklesNotifer so that all settings can persist
    Closes issues #2

--- src/main/java/hudson/plugins/pickles/PicklesNotifier.java
+++ src/main/java/hudson/plugins/pickles/PicklesNotifier.java
@@ -15,12 +15,48 @@ import hudson.tasks.Publisher;
 import hudson.tools.ToolInstallation;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class PicklesNotifier extends Notifier {
+
     @DataBoundConstructor
-    public PicklesNotifier(){}
+    public PicklesNotifier(){ }
+
+    private String picklesInstallationName;
+    private String featureDirectory;
+    private String outputDirectory;
+    private String testResultFormat;
+    private String documentationFormat;
+    private String systemUnderTestName;
+    private String systemUnderTestVersion;
+    private String linkResultsFiles;
+    private Boolean enableComments;
+    private Boolean includeExperimentalFeatures;
+
+    public String getFeatureDirectory() { return featureDirectory; }
+    public String getOutputDirectory() { return outputDirectory; }
+    public String getTestResultFormat(){ return testResultFormat; }
+    public String getDocumentationFormat() { return documentationFormat; }
+    public String getSystemUnderTestName() { return systemUnderTestName; }
+    public String getSystemUnderTestVersion() { return systemUnderTestVersion; }
+    public String getLinkResultsFiles() { return linkResultsFiles; }
+    public Boolean getEnableComments() { return enableComments; }
+    public Boolean getIncludeExperimentalFeatures() { return includeExperimentalFeatures; }
+    public String getPicklesInstallationName() { return picklesInstallationName; }
+
+    @DataBoundSetter public void setFeatureDirectory(String featureDirectory) { this.featureDirectory = featureDirectory; }
+    @DataBoundSetter public void setOutputDirectory(String outputDirectory) { this.outputDirectory = outputDirectory; }
+    @DataBoundSetter public void setTestResultFormat(String testResultFormat) { this.testResultFormat = testResultFormat; }
+    @DataBoundSetter public void setDocumentationFormat(String documentationFormat) { this.documentationFormat = documentationFormat; }
+    @DataBoundSetter public void setSystemUnderTestName(String systemUnderTestName) { this.systemUnderTestName = systemUnderTestName; }
+    @DataBoundSetter public void setSystemUnderTestVersion(String systemUnderTestVersion) { this.systemUnderTestVersion = systemUnderTestVersion; }
+    @DataBoundSetter public void setLinkResultsFiles(String linkResultsFiles) { this.linkResultsFiles = linkResultsFiles; }
+    @DataBoundSetter public void setEnableComments(Boolean enableComments) { this.enableComments = enableComments; }
+    @DataBoundSetter public void setIncludeExperimentalFeatures(Boolean includeExperimentalFeatures) { this.includeExperimentalFeatures = includeExperimentalFeatures; }
+    @DataBoundSetter public void setPicklesInstallationName(String picklesInstallationName) { this.picklesInstallationName = picklesInstallationName; }
 
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
@@ -42,9 +78,6 @@ public class PicklesNotifier extends Notifier {
             load();
         }
 
-        /**
-         * Human readable name of this kind of configurable object.
-         */
         @Override
         public String getDisplayName() {
             return "Pickles";
@@ -56,10 +89,10 @@ public class PicklesNotifier extends Notifier {
         }
 
         public PicklesToolInstallation[] getInstallations() {
-            return installations;
+            return Arrays.copyOf(installations, installations.length);
         }
 
-        public void setInstallations(PicklesToolInstallation[] installations) {
+        public void setInstallations(PicklesToolInstallation... installations) {
             this.installations = installations;
             save();
         }
